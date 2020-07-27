b3a9dd3e9df41aac20abfd4294924ccb01ab66f4
1160300919 <1034538932@qq.com>
2020-07-27 09:34:48 +0800

    项目第一次提交

---
+++ SmartBuy/src/test/java/com/hit/sb/SmartBuyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hit.sb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartBuyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
