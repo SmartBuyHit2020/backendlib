4cde4e4c6f642a8879acc89fda415459b1f7360b
1160300919 <1034538932@qq.com>
2020-07-29 10:59:12 +0800

    加入商品表，修复了点问题

--- SmartBuy/src/main/java/com/hit/sb/SmartBuyApplication.java
+++ SmartBuy/src/main/java/com/hit/sb/SmartBuyApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-@MapperScan(basePackages={"com.hit.sb.hr.mapper","com.hit.sb.admin.mapper","com.hit.sb.cart.mapper","com.hit.sb.goods.mapper"})
+@MapperScan(basePackages={"com.hit.sb.hr.mapper","com.hit.sb.admin.mapper","com.hit.sb.cart.mapper","com.hit.sb.goods.mapper","com.hit.sb.order.mapper"})
 public class SmartBuyApplication {
 
 	public static void main(String[] args) {
